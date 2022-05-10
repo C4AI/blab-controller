c046be337904e8891f43e4750245200be2c43829
Vinícius <matosvb@gmail.com>
2022-05-10 09:24:11 -0300

    Fix log path

--- blab-controller/controller/settings/base.py
+++ blab-controller/controller/settings/base.py
@@ -14,6 +14,7 @@ from typing import Any
 import structlog
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# BASE_DIR is the directory that contains manage.py
 
 DEBUG = False
 
@@ -129,49 +130,49 @@ REST_FRAMEWORK = {
 # Logging
 
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "json_formatter": {
-            "()": structlog.stdlib.ProcessorFormatter,
-            "processor": structlog.processors.JSONRenderer(),
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'json_formatter': {
+            '()': structlog.stdlib.ProcessorFormatter,
+            'processor': structlog.processors.JSONRenderer(),
         },
-        "plain_console": {
-            "()": structlog.stdlib.ProcessorFormatter,
-            "processor": structlog.dev.ConsoleRenderer(),
+        'plain_console': {
+            '()': structlog.stdlib.ProcessorFormatter,
+            'processor': structlog.dev.ConsoleRenderer(),
         },
-        "key_value": {
-            "()": structlog.stdlib.ProcessorFormatter,
-            "processor": structlog.processors.KeyValueRenderer(
+        'key_value': {
+            '()': structlog.stdlib.ProcessorFormatter,
+            'processor': structlog.processors.KeyValueRenderer(
                 key_order=['timestamp', 'level', 'event', 'logger']
             ),
         },
     },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "plain_console",
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'plain_console',
         },
-        "json_file": {
-            "class": "logging.handlers.WatchedFileHandler",
-            "filename": ".logs/django_json.log",
-            "formatter": "json_formatter",
+        'json_file': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': str(BASE_DIR / '.logs' / 'django_json.log'),
+            'formatter': 'json_formatter',
         },
-        "flat_line_file": {
-            "class": "logging.handlers.WatchedFileHandler",
-            "filename": ".logs/django_flat.log",
-            "formatter": "key_value",
+        'flat_line_file': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': str(BASE_DIR / '.logs' / 'django_flat.log'),
+            'formatter': 'key_value',
         },
     },
-    "loggers": {
-        "django_structlog": {
-            "handlers": ["console", "flat_line_file", "json_file"],
-            "level": "INFO",
+    'loggers': {
+        'django_structlog': {
+            'handlers': ['console', 'flat_line_file', 'json_file'],
+            'level': 'INFO',
         },
         # Make sure to replace the following logger's name for yours
-        "blab_controller": {
-            "handlers": ["console", "flat_line_file", "json_file"],
-            "level": "INFO",
+        'blab_controller': {
+            'handlers': ['console', 'flat_line_file', 'json_file'],
+            'level': 'INFO',
         },
     },
 }
@@ -179,7 +180,7 @@ LOGGING = {
 structlog.configure(
     processors=[
         structlog.stdlib.filter_by_level,
-        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.TimeStamper(fmt='iso'),
         structlog.stdlib.add_logger_name,
         structlog.stdlib.add_log_level,
         structlog.stdlib.PositionalArgumentsFormatter(),
